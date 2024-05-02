1075a65254a6232f53284d9e166115486007d741
CarolGonzaga <dev.carolgonzaga@gmail.com>
2024-05-02 19:05:05 -0300

    alteracao de limite de caracteres

--- modules/post/post.model.js
+++ modules/post/post.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import User from "../user/user.model"
 
 const PostSchema = new mongoose.Schema({
-  text: { type: String, required: true, maxlength: 500 },
+  text: { type: String, required: true, maxlength: 256 },
   createdBy: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   createdDate: { type: Date, required: true }
 })
