d6c27cc4e6985d62ef0ad78202480050d633baa8
Carol Gonzaga <dev.carolgonzaga@gmail.com>
2025-10-16 15:15:49 -0300

    fix: Corrige tamanho do H1

--- src/components/typography/H1.jsx
+++ src/components/typography/H1.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const H1 = styled.h1`
   font-family: "Barlow Condensed", sans-serif;
-  font-size: 38px;
+  font-size: 34px;
   font-weight: 500;
   text-align: center;
   text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
@@ -12,7 +12,7 @@ const H1 = styled.h1`
   margin-top: 50px;
 
   @media (min-width: ${(props) => props.theme.WIDTH_XL}) {
-    font-size: 70px;
+    font-size: 64px;
     height: 60px;
     line-height: 40px;
     margin-top: 0;
@@ -20,7 +20,7 @@ const H1 = styled.h1`
 
   @media (min-height: ${(props) => props.theme.HEIGHT_XL}) and
   (min-width: 760px) {
-    font-size: 60px;
+    font-size: 54px;
     height: 60px;
   }
 `
